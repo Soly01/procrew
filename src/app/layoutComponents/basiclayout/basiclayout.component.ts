28ed6f3124889cb2b364726013ba8182faa85891
Soly01 <solimana425@yahoo.com>
2025-02-28 14:20:47 +0200

    making footer

--- src/app/layoutComponents/basiclayout/basiclayout.component.ts
+++ src/app/layoutComponents/basiclayout/basiclayout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../Components/footer/footer.component';
 
 @Component({
   selector: 'app-basiclayout',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './basiclayout.component.html',
   styleUrl: './basiclayout.component.scss',
 })
